34373a14818d3492b65884f42f45da28e7e6877a
Adrian Torchiana <adrian.torchiana@gmail.com>
2020-09-23 15:27:35 -0700

    Graphs

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # finance_playground
+
+![S&P 500 Close](s&p500_close.png)
+
+![S&P 500 Log Returns](s&p500_log_return.png)
--- explore.R
+++ explore.R
@@ -12,18 +12,21 @@ df$log_return <- c(NA, diff(log(df$close)))
 p <- (ggplot(df, aes(x=date, y=close)) +
       geom_line() +
       ylab("closing price") +
-      scale_x_date("", date_labels="%b %Y") +
+      scale_x_date("", date_labels="%b %Y", breaks="4 years") +
       theme_bw() +
       theme(plot.title = element_text(hjust = 0.5)) +
       ggtitle("S&P 500"))
-p
+filename <- "s&p500_close.png"
+ggsave(filename, plot=p, width=10, height=8)
 
 p <- (ggplot(df, aes(x=date, y=log_return)) +
       geom_point(alpha=0.5) +
       ylab("log returns (close-to-close)") +
+      scale_x_date("", date_labels="%b %Y", breaks="4 years") +
       theme_bw() +
       theme(plot.title = element_text(hjust = 0.5)) +
       ggtitle("S&P 500"))
-p
+filename <- "s&p500_log_return.png"
+ggsave(filename, plot=p, width=10, height=8)
 
 
