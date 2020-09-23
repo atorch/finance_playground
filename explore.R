7000d95457d5d5e8a60f564ffd8db41d169c5e18
Adrian Torchiana <adrian.torchiana@gmail.com>
2020-09-23 15:51:11 -0700

    Vanilla model (iid normal by max likelihood)

--- explore.R
+++ explore.R
@@ -29,4 +29,14 @@ p <- (ggplot(df, aes(x=date, y=log_return)) +
 filename <- "s&p500_log_return.png"
 ggsave(filename, plot=p, width=10, height=8)
 
+cutoff_date <- as.Date("2010-01-01")
+train <- df[df$date < cutoff_date, ]
+test <- df[df$date >= cutoff_date, ]
 
+## Let's fit a simple model to the training set
+## and evaluate its log likelihood on the test set
+train_mean <- mean(train$log_return, na.rm=TRUE)
+train_var <- var(train$log_return, na.rm=TRUE)
+
+## TODO Try some other vanilla models: iid student's t, mixture of normals, HMM, etc etc
+log_likelihood_iid_normal <- mean(dnorm(test$log_return, mean=train_mean, sd=sqrt(train_var), log=TRUE))
